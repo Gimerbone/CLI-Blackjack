10e23c08dc84237aca7f0e73de13469faa3bee41
Gimerbone <gimerbone@gmail.com>
2024-12-13 22:23:35 +0700

    Setting Up Github

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+
+/**
+ * Inisiasi Game
+ *      1. Kasih 2 Kartu kpd Player dan Dealer
+ *      2. Menginfokan kartu player dan kartu dealer A
+ *      3. Memberikan Player
+ * Jalan Game
+ * Ending Game
+ */
+int main()
+{
+
+}
